993671e747c100582b06acc85ae20570a0cc07f4
杨肖铭 <651260437@qq.com>
2024-09-12 16:13:46 +0800

    面试羊刷题网站 - init

---
+++ src/main/java/com/xm/mianshiyoung/job/once/FullSyncPostToEs.java
@@ -0,0 +1,46 @@
+package com.xm.mianshiyoung.job.once;
+
+import com.xm.mianshiyoung.esdao.PostEsDao;
+import com.xm.mianshiyoung.model.dto.post.PostEsDTO;
+import com.xm.mianshiyoung.model.entity.Post;
+import com.xm.mianshiyoung.service.PostService;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import cn.hutool.core.collection.CollUtil;
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * 全量同步帖子到 es
+ *
+ */
+// todo 取消注释开启任务
+//@Component
+@Slf4j
+public class FullSyncPostToEs implements CommandLineRunner {
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private PostEsDao postEsDao;
+
+    @Override
+    public void run(String... args) {
+        List<Post> postList = postService.list();
+        if (CollUtil.isEmpty(postList)) {
+            return;
+        }
+        List<PostEsDTO> postEsDTOList = postList.stream().map(PostEsDTO::objToDto).collect(Collectors.toList());
+        final int pageSize = 500;
+        int total = postEsDTOList.size();
+        log.info("FullSyncPostToEs start, total {}", total);
+        for (int i = 0; i < total; i += pageSize) {
+            int end = Math.min(i + pageSize, total);
+            log.info("sync from {} to {}", i, end);
+            postEsDao.saveAll(postEsDTOList.subList(i, end));
+        }
+        log.info("FullSyncPostToEs end, total {}", total);
+    }
+}
---
+++ src/main/java/com/xm/mianshiyoung/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.xm.mianshiyoung.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xm.mianshiyoung.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
